02b6192198131ce3735e8994343dacd413bf22c0
Daniel <Daniel.Milholland@icloud.com>
2016-10-23 18:42:39 -0500

    Phase 2 Refactoring

--- message.js
+++ message.js
@@ -7,262 +7,246 @@ let queue = {}
 
 exports.cmds = (msg) => {
   var message = msg.content.toLowerCase()
-    // Commands
-  if (message === '!commands') {
-    var text = `List of Commands:
-            !ping - Replys Pong
-            !coin - Flip a coin
-            !dice (opt X) - Roll the dice (x)
-            !chuck - Chuck Norris Joke
-            !toast - Prints Toast
-            !slap @user - Slaps all mentioned users
-            !insult (@user - optional) - Insults the sender or @user.
-            !cat - Random Cat
-            !boom - Roast your fellow users
-            !to_C <#> - Converts Fahrenheit to Celsius
-            !to_F <#> - Converts Celsius to Fahrenheit
-            !time <TIMEZONE> - Returns current time in zone. Ex: !time CST`
-    // If on Milhound's Server add the following commands
-    if (msg.guild.id === '167693566267752449') {
-      text += `
-            !gamer to add/remove Gamer role.
-            !programmer to add/remove Programmer role.
-            
-            IN BETA:
-            !play <url> - Plays a song from YouTube.
-            !pause - Pauses song
-            !resume - Resumes song
-            !volume - Tells you current volume
-            !volume+ - Increases volume by 10%
-            !volume- - Reduces volume by 10% ` }
-    msg.channel.sendMessage(text)
-  }
-
-  // Gamer Command
-  if (message === '!gamer' && msg.guild.id === '167693566267752449') {
-    fn.toggleRole(msg, '235440340981514240')
-  }
-
-  // Programmer Command
-  if (message === '!programmer' && msg.guild.id === '167693566267752449') {
-    fn.toggleRole(msg, '235562658877800448')
-  }
-
-  // Mute Command
-  if (message.startsWith('!mute') && msg.guild.member(msg.author).hasPermission('MUTE_MEMBERS')) {
-    for (var muteMention of msg.mentions.users.array()) {
-      msg.guild.member(muteMention).setMute(true)
-      msg.reply(muteMention + ' has been globally muted!')
-    }
-  }
-
-  // Mute Command
-  if (message.startsWith('!unmute') && msg.guild.member(msg.author).hasPermission('MUTE_MEMBERS')) {
-    for (var unmuteMention of msg.mentions.users.array()) {
-      msg.guild.member(unmuteMention).setMute(false)
-      msg.reply(unmuteMention + ' has been unmuted!')
-    }
-  }
-
-  // Ping Command
-  if (message === '!ping') {
-    msg.channel.sendMessage('pong')
-  }
-
-  // Coin Flip
-  if (message === '!coin') {
-    if (Math.floor(Math.random() * 2 + 1) === 1) {
-      msg.channel.sendMessage('Heads')
-    } else {
-      msg.channel.sendMessage('Tails')
-    }
-  }
-
-  // Dice
-  if (message.startsWith('!dice') || message.startsWith('!roll')) {
-    var argsDice = message.split(' ')
-    if (argsDice[1] !== undefined) {
-      msg.channel.sendMessage(Math.floor(Math.random() * parseInt(argsDice[1]) + 1))
-    } else {
-      msg.channel.sendFile('./img/dice' + Math.floor((Math.random() * 6) + 1) + '.png')
-    }
-  }
-
-  // Slap Command
-  if (message.startsWith('!slap') && msg.mentions.users.array().length >= 0) {
-    for (var slapTarget of msg.mentions.users.array()) {
-      msg.channel.sendMessage(slapTarget + ' You\'ve been SLAPPED!')
-    }
-    msg.delete
-  }
-
-  // Cat Command
-  if (message === '!cat') {
-    fn.apiRequest('http://random.cat/meow').then(response => msg.channel.sendMessage(response.file))
-  }
-
-  // Insult Command
-  if (message.startsWith('!insult') && msg.mentions.users.array().length >= 0) {
-    for (var insultTarget of msg.mentions.users.array()) {
-      fn.apiRequest('https://quandyfactory.com/insult/json').then(response =>
-        msg.channel.sendMessage(insultTarget + ' ' + response.insult))
-    }
-  }
-
-  // Celsius to Fahrenheit
-  if (message.startsWith('!to_F')) {
-    var argsF = message.split(' ')
-    if (argsF[1] && !argsF[2]) {
-      var fromC = argsF[1]
-      // Round to whole number
-      var toF = (fromC * 1.8 + 32).toFixed(0)
-      msg.reply(toF)
-    }
-  }
-
-  // Fahrenheit to Celsius
-  if (message.startsWith('!to_C')) {
-    var argsC = message.split(' ')
-    if (argsC[1] && !argsC[2]) {
-      var fromF = argsC[1]
-      // Round to whole number
-      var toC = ((fromF - 32) * (5 / 9)).toFixed(0)
-      msg.reply(toC)
-    }
-  }
-
-  // Time
-  if (message.startsWith('!time')) {
-    var argsTime = message.split(' ')
-    // Variable to confirm all calculations suceeded
-    var goodTime = true
-    var date = new Date()
-    var hour = date.getUTCHours()
-
-    switch (argsTime[1].toLowerCase()) {
-      // United States
-      case 'pdt':
-      case 'pst':
-      case 'california':
-        hour = hour - 7
-        break
-
-      case 'mdt':
-      case 'mst':
-        hour = hour - 6
-        break
-
-      case 'cdt':
-      case 'cst':
-      case 'texas':
-        hour = hour - 5
-        break
-
-      case 'edt':
-      case 'est':
-        hour = hour - 4
-        break
-
-      case 'hst':
-      case 'hawaii':
-        hour = hour - 10
-        break
-
-      // Rio de Janeiro
-      case 'brt':
-        hour = hour - 3
-        break
-
-      // Europe
-      case 'bst':
-      case 'west':
-      case 'cet':
-      case 'london':
-        hour = hour + 1
-        break
-
-      case 'cest':
-      case 'sweden':
-      case 'eet':
-      case 'germany':
-      case 'austria':
-        hour = hour + 2
-        break
-
-      case 'eest':
-      case 'finland':
-        hour = hour + 3
-        break
-
-      case 'ist':
-      case 'india':
-        hour = Math.floor(hour + 5.5)
-        break
-
-      case 'sgt':
-      case 'singapore':
-        hour = hour + 8
-        break
-
-      case 'jst':
-      case 'japan':
-      case 'tokyo':
-        hour = hour + 9
-        break
-
-      case 'aedt':
-      case 'australia':
-        hour = hour + 11
-        break
-
-      case 'nzdt':
-      case 'new-zealand':
-        hour = hour + 13
-        break
-
-      default:
-        // Allow users to do custom GMT/UTC timezones with GMT+1 as an example
-        if (argsTime[1].toLowerCase() === 'gmt' || argsTime[1].toLowerCase() === 'utc') {
-          if (argsTime[2] !== undefined) {
-            var modifier = argsTime[2]
-            console.log(modifier)
-            // Grab the + or - from properly formated command
-            if (modifier.slice(0, 1) === '-') {
-              hour = hour - parseInt(modifier.slice(1))
-            } else {
-              hour = hour + parseInt(modifier)
-            }
-          }
-        } else {
-          // All other checks failed, its is not a timezone currently in code.
-          msg.reply('Unknown Timezone.')
-          goodTime = false
-          // Log passed timezone for a potential addition
-          console.log('Timezone not avaliable yet: ' + argsTime[1])
-        }
-    }
-    if (hour < 0) {
-      hour = 24 + hour
-    }
-    if (hour > 24) {
-      hour = hour - 24
-    }
-    if (hour < 10) {
-      hour = '0' + hour
-    }
-    var minutes = date.getUTCMinutes()
-    if (minutes < 10) {
-      minutes = '0' + minutes
-    }
-    // Confirm all tasks are complete by adding a slight delay.
-    setTimeout(() => {
-      if (goodTime) {
-        msg.reply(hour + ':' + minutes)
-      }
-    }, 500)
-  }
-
   const commands = {
+    'commands': (msg) => {
+      var text = `List of Commands:
+        !ping - Replys Pong
+        !coin - Flip a coin
+        !dice (opt X) - Roll the dice (x)
+        !chuck - Chuck Norris Joke
+        !toast - Prints Toast
+        !slap @user - Slaps all mentioned users
+        !insult (@user - optional) - Insults the sender or @user.
+        !cat - Random Cat
+        !boom - Roast your fellow users
+        !to_C <#> - Converts Fahrenheit to Celsius
+        !to_F <#> - Converts Celsius to Fahrenheit
+        !time <TIMEZONE> - Returns current time in zone. Ex: !time CST`
+      // If on Milhound's Server add the following commands
+      if (msg.guild.id === '167693566267752449') {
+        text += `
+        !gamer to add/remove Gamer role.
+        !programmer to add/remove Programmer role.
+        
+        IN BETA:
+        !play <url> - Plays a song from YouTube.
+        !pause - Pauses song
+        !resume - Resumes song
+        !volume - Tells you current volume
+        !volume+ - Increases volume by 10%
+        !volume- - Reduces volume by 10% ` }
+      msg.channel.sendMessage(text)
+    },
+    'time': (msg) => {
+      var argsTime = message.split(' ')
+      // Variable to confirm all calculations suceeded
+      var goodTime = true
+      var date = new Date()
+      var hour = date.getUTCHours()
+
+      switch (argsTime[1].toLowerCase()) {
+        // United States
+        case 'pdt':
+        case 'pst':
+        case 'california':
+          hour = hour - 7
+          break
+
+        case 'mdt':
+        case 'mst':
+          hour = hour - 6
+          break
+
+        case 'cdt':
+        case 'cst':
+        case 'texas':
+          hour = hour - 5
+          break
+
+        case 'edt':
+        case 'est':
+          hour = hour - 4
+          break
+
+        case 'hst':
+        case 'hawaii':
+          hour = hour - 10
+          break
+
+        // Rio de Janeiro
+        case 'brt':
+          hour = hour - 3
+          break
+
+        // Europe
+        case 'bst':
+        case 'west':
+        case 'cet':
+        case 'london':
+          hour = hour + 1
+          break
+
+        case 'cest':
+        case 'sweden':
+        case 'eet':
+        case 'germany':
+        case 'austria':
+          hour = hour + 2
+          break
+
+        case 'eest':
+        case 'finland':
+          hour = hour + 3
+          break
+
+        case 'ist':
+        case 'india':
+          hour = Math.floor(hour + 5.5)
+          break
+
+        case 'sgt':
+        case 'singapore':
+          hour = hour + 8
+          break
+
+        case 'jst':
+        case 'japan':
+        case 'tokyo':
+          hour = hour + 9
+          break
+
+        case 'aedt':
+        case 'australia':
+          hour = hour + 11
+          break
+
+        case 'nzdt':
+        case 'new-zealand':
+          hour = hour + 13
+          break
+
+        default:
+          // Allow users to do custom GMT/UTC timezones with GMT+1 as an example
+          if (argsTime[1].toLowerCase() === 'gmt' || argsTime[1].toLowerCase() === 'utc') {
+            if (argsTime[2] !== undefined) {
+              var modifier = argsTime[2]
+              console.log(modifier)
+              // Grab the + or - from properly formated command
+              if (modifier.slice(0, 1) === '-') {
+                hour = hour - parseInt(modifier.slice(1))
+              } else {
+                hour = hour + parseInt(modifier)
+              }
+            }
+          } else {
+            // All other checks failed, its is not a timezone currently in code.
+            msg.reply('Unknown Timezone.')
+            goodTime = false
+            // Log passed timezone for a potential addition
+            console.log('Timezone not avaliable yet: ' + argsTime[1])
+          }
+      }
+      if (hour < 0) {
+        hour = 24 + hour
+      }
+      if (hour > 24) {
+        hour = hour - 24
+      }
+      if (hour < 10) {
+        hour = '0' + hour
+      }
+      var minutes = date.getUTCMinutes()
+      if (minutes < 10) {
+        minutes = '0' + minutes
+      }
+      // Confirm all tasks are complete by adding a slight delay.
+      setTimeout(() => {
+        if (goodTime) {
+          msg.reply(hour + ':' + minutes)
+        }
+      }, 500)
+    },
+    'mute': (msg) => {
+      if (msg.guild.member(msg.author).hasPermission('MUTE_MEMBERS')) {
+        for (var muteMention of msg.mentions.users.array()) {
+          msg.guild.member(muteMention).setMute(true)
+          msg.reply(muteMention + ' has been globally muted!')
+        }
+      }
+    },
+    'unmute': (msg) => {
+      if (msg.guild.member(msg.author).hasPermission('MUTE_MEMBERS')) {
+        for (var unmuteMention of msg.mentions.users.array()) {
+          msg.guild.member(unmuteMention).setMute(false)
+          msg.reply(unmuteMention + ' has been unmuted!')
+        }
+      }
+    },
+    'programmer': (msg) => {
+      if (msg.guild.id === '167693566267752449') {
+        fn.toggleRole(msg, '235562658877800448')
+      }
+    },
+    'gamer': (msg) => {
+      if (msg.guild.id === '167693566267752449') {
+        fn.toggleRole(msg, '235440340981514240')
+      }
+    },
+    'ping': (msg) => {
+      msg.channel.sendMessage('Pong!')
+    },
+    'coin': (msg) => {
+      if (Math.floor(Math.random() * 2 + 1) === 1) {
+        msg.channel.sendMessage('Heads')
+      } else {
+        msg.channel.sendMessage('Tails')
+      }
+    },
+    'dice': (msg) => {
+      var argsDice = message.split(' ')
+      if (argsDice[1] !== undefined) {
+        msg.channel.sendMessage(Math.floor(Math.random() * parseInt(argsDice[1]) + 1))
+      } else {
+        msg.channel.sendFile('./img/dice' + Math.floor((Math.random() * 6) + 1) + '.png')
+      }
+    },
+    'slap': (msg) => {
+      if (msg.mentions.users.array().length >= 0) {
+        for (var slapTarget of msg.mentions.users.array()) {
+          msg.channel.sendMessage(slapTarget + ' You\'ve been SLAPPED!')
+        }
+        msg.delete
+      }
+    },
+    'cat': (msg) => {
+      fn.apiRequest('http://random.cat/meow').then(response => msg.channel.sendMessage(response.file))
+    },
+    'insult': (msg) => {
+      if (msg.mentions.users.array().length >= 0) {
+        for (var insultTarget of msg.mentions.users.array()) {
+          fn.apiRequest('https://quandyfactory.com/insult/json').then(response =>
+            msg.channel.sendMessage(insultTarget + ' ' + response.insult))
+        }
+      }
+    },
+    'to_f': (msg) => {
+      var argsF = message.split(' ')
+      if (argsF[1] && !argsF[2]) {
+        var fromC = argsF[1]
+        // Round to whole number
+        var toF = (fromC * 1.8 + 32).toFixed(0)
+        msg.reply(toF)
+      }
+    },
+    'to_c': (msg) => {
+      var argsC = message.split(' ')
+      if (argsC[1] && !argsC[2]) {
+        var fromF = argsC[1]
+        // Round to whole number
+        var toC = ((fromF - 32) * (5 / 9)).toFixed(0)
+        msg.reply(toC)
+      }
+    },
     'wipe': (msg) => {
       if (msg.guild.member(msg.author).hasPermission('MANAGE_MESSAGES')) {
         var argsWipe = message.split(' ')
