b02b5a66a029fcc8042b7d98f05d612c14af2898
Daniel Milholland <Daniel Milholland>
2016-10-24 11:35:53 -0500

    Fix for !play <url>

--- message.js
+++ message.js
@@ -309,6 +309,7 @@ exports.cmds = (msg) => {
         }
         queue[msg.guild.id].songs.push({url: url, title: info.title, requester: msg.author.username})
         msg.channel.sendMessage(`Added **${info.title}** to queue.`)
+        if (queue[msg.guild.id].playing === false) commands.play(msg, false)
       })
     },
     'join': (msg) => {
@@ -330,8 +331,8 @@ exports.cmds = (msg) => {
       ${(currentQueue > 5) ? '*[Only next 5 shown]*' : ''}
     `)
     },
-    'play': (msg) => {
-      if (message.indexOf('http') !== -1) commands.add(msg)
+    'play': (msg, retry) => {
+      if (message.indexOf('http') !== -1 && retry !== false) return commands.add(msg)
       if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play(msg))
       if (!msg.guild.voiceConnection) {
         var voiceChannel = msg.member.voiceChannel
