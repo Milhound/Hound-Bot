d56623d14d12ed466e13a81bd43df9d37038bdee
Daniel <Daniel.Milholland@icloud.com>
2016-10-25 18:36:44 -0500

    using Math.pow for ES2016

--- functions.js
+++ functions.js
@@ -75,7 +75,7 @@ exports.addLevel = (msg) => {
 }
 
 function getExpFromLevel (level) {
-  return 5 * (level ** 2) + 50 * level + 100
+  return 5 * (Math.pow(level, 2)) + 50 * level + 100
 }
 
 function getLevelFromExp (exp) {
