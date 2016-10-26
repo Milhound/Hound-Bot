8e8a8b08307837e61a5476667654016679a687fa
Milhound <Milhound@icloud.com>
2016-10-25 21:42:46 -0500

    Moved time to functions

--- functions.js
+++ functions.js
@@ -27,6 +27,136 @@ exports.apiRequest = (url, callback) => {
   })
 }
 
+exports.getTime = (msg) => {
+  var argsTime = msg.split(' ')
+  // Variable to confirm all calculations suceeded
+  var goodTime = true
+  var date = new Date()
+  var hour = date.getUTCHours()
+
+  switch (argsTime[1].toLowerCase()) {
+    // United States
+    case 'pdt':
+    case 'pst':
+    case 'california':
+      hour = hour - 7
+      break
+
+    case 'mdt':
+    case 'mst':
+      hour = hour - 6
+      break
+
+    case 'cdt':
+    case 'cst':
+    case 'texas':
+      hour = hour - 5
+      break
+
+    case 'edt':
+    case 'est':
+      hour = hour - 4
+      break
+
+    case 'hst':
+    case 'hawaii':
+      hour = hour - 10
+      break
+
+    // Rio de Janeiro
+    case 'brt':
+      hour = hour - 3
+      break
+
+    // Europe
+    case 'bst':
+    case 'west':
+    case 'cet':
+    case 'london':
+      hour = hour + 1
+      break
+
+    case 'cest':
+    case 'sweden':
+    case 'eet':
+    case 'germany':
+    case 'austria':
+      hour = hour + 2
+      break
+
+    case 'eest':
+    case 'finland':
+      hour = hour + 3
+      break
+
+    case 'ist':
+    case 'india':
+      hour = Math.floor(hour + 5.5)
+      break
+
+    case 'sgt':
+    case 'singapore':
+      hour = hour + 8
+      break
+
+    case 'jst':
+    case 'japan':
+    case 'tokyo':
+      hour = hour + 9
+      break
+
+    case 'aedt':
+    case 'australia':
+      hour = hour + 11
+      break
+
+    case 'nzdt':
+    case 'new-zealand':
+      hour = hour + 13
+      break
+
+    default:
+      // Allow users to do custom GMT/UTC timezones with GMT+1 as an example
+      if (argsTime[1].toLowerCase() === 'gmt' || argsTime[1].toLowerCase() === 'utc') {
+        if (argsTime[2] !== undefined) {
+          var modifier = argsTime[2]
+          console.log(modifier)
+          // Grab the + or - from properly formated command
+          if (modifier.slice(0, 1) === '-') {
+            hour = hour - parseInt(modifier.slice(1))
+          } else {
+            hour = hour + parseInt(modifier)
+          }
+        }
+      } else {
+        // All other checks failed, its is not a timezone currently in code.
+        msg.reply('Unknown Timezone.')
+        goodTime = false
+        // Log passed timezone for a potential addition
+        console.log('Timezone not avaliable yet: ' + argsTime[1])
+      }
+  }
+  if (hour < 0) {
+    hour = 24 + hour
+  }
+  if (hour > 24) {
+    hour = hour - 24
+  }
+  if (hour < 10) {
+    hour = '0' + hour
+  }
+  var minutes = date.getUTCMinutes()
+  if (minutes < 10) {
+    minutes = '0' + minutes
+  }
+  // Confirm all tasks are complete by adding a slight delay.
+  setTimeout(() => {
+    if (goodTime) {
+      msg.reply(hour + ':' + minutes)
+    }
+  }, 500)
+}
+
 exports.addExperience = (msg) => {
   if (!expLocked.hasOwnProperty(msg.author.id)) expLocked[msg.author.id] = false
   if (expLocked[msg.author.id] === false) {
