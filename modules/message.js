e274285e51efea3b153505671300f354569e87a0
Milhound <Daniel.Milholland@icloud.com>
2016-12-25 20:44:11 -0600

    fix for ud error thanks sys0

--- modules/message.js
+++ modules/message.js
@@ -230,7 +230,7 @@ exports.cmds = (msg) => {
       Guild.toggleGreet(msg)
     },
     'ud': (msg) => {
-      Fn.apiRequest('http://api.urbandictionary.com/v0/define?term='+msg.split('ud ')[1]).then(response => msg.channel.sendMessage(Fn.formatUDString(response, msg.split('ud ')[1])))
+      Fn.apiRequest('http://api.urbandictionary.com/v0/define?term='+msg.content.split('ud ')[1]).then(response => msg.channel.sendMessage(Fn.formatUDString(response, msg.split('ud ')[1])))
     }
   }
 
