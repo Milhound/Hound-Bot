1d1d9f39c2d94c2a3660111f33a171e167914d33
Daniel Milholland <Milhound@MacBook-Pro.local>
2017-09-09 12:59:17 -0500

    updated guild id to string

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ bot.on('guildMemberAdd', member => {
     // Send welcome message
     Usr.welcomeMessage(member)
   }
-  if (member.guild.id === 149632998055215105) {
+  if (member.guild.id === '149632998055215105') {
+    console.log('New user joined TGC')
     const channel = member.guild.channels.find('id', '160062431236849665')
     if (!channel) console.log('Channel not found!')
     channel.send(`Welcome to TGC ${member}!`)
