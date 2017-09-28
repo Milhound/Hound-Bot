215bd1980e663d753e41cd86d1232538b9ef6ccd
Milhound <milhound@gmail.com>
2017-09-27 22:31:29 -0500

    possible fix for msg not passing

--- modules/voice.js
+++ modules/voice.js
@@ -137,7 +137,7 @@ function play (msg, song) {
     } else if (m.content.startsWith('!resume')) {
       msg.channel.send('Resuming...').then(() => { dispatcher.resume() })
     } else if (m.content === '!skip') {
-      msg.channel.send('Skipping').then((msg) => { dispatcher.end(msg) })
+      msg.channel.send('Skipping').then((msg) => { dispatcher.end() })
     } else if (m.content === '!volume+') {
       dispatcher.setVolume(dispatcher.volume + (dispatcher.volume / 4))
       msg.channel.send(`Volume set to ${Math.floor(dispatcher.volume * 1000)}%`)
@@ -155,7 +155,7 @@ function play (msg, song) {
       dispatcher.end(msg)
     }
   })
-    dispatcher.on('end', (msg) => {
+    dispatcher.on('end', () => {
       collector.stop()
       var song = queue[msg.guild.id].songs.shift()
       play(msg, song)
