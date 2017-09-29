3e7c3d852cdecc80c8cbccdc88aaf4993a9875da
Milhound <milhound@gmail.com>
2017-09-29 11:40:57 -0500

    close timeout

--- modules/voice.js
+++ modules/voice.js
@@ -121,7 +121,7 @@ function play (msg) {
   setTimeout( () => {
     queue[msg.guild.id].songs.shift()
     msg.channel.send(`Playing: **${song.title}** as requested by: ${song.requester}`, 1000)
-  
+  })
   dispatcher = connection.playStream(yt(song.url,
     {filter: 'audioonly'}).on('error', (err) => {
       if (err.code === 'ECONNRESET') return
