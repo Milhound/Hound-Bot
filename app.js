785fe3b247f425191d4528480919ca6ca4f5ae30
Daniel Milholland <Milhound@icloud.com>
2019-06-11 12:51:31 -0500

    conversion

--- app.js
+++ app.js
@@ -6,34 +6,12 @@ const Guild = require('./modules/guild.js')
 const Config = require('./data/config.json')
 
 bot.on('ready', () => {
-  console.log('Bot is Online')
+  console.log(`Logged in as ${client.user.tag}!`)
   Usr.initiateSave()
   Guild.initGuildSave()
 })
 
-bot.on('guildMemberAdd', member => {
-  if (Config.server.id === member.guild.id) {
-    // Log user
-    Usr.logUser(member)
-    // Send welcome message
-    Usr.welcomeMessage(member)
-  }
-  if (member.guild.id === '149632998055215105') {
-    console.log('New user joined TGC')
-    const channel = member.guild.channels.find('id', '149632998055215105')
-    if (!channel) console.log('Channel not found!')
-    channel.send(`Welcome to TGC ${member}!`)
-  }
-})
-
-bot.on('guildMemberRemove', member => {
-  if (Config.server.id === member.guild.id) {
-    Usr.logUserLeave(member)
-  }
-})
-
 bot.on('message', message => {
-  if (message.channel.type === 'dm' && message.author.id !== Config.id) return message.reply('I currently cannot communicate via DM. Please use !commands in a regular server channel.')
   Usr.addExperience(message)
   if (!message.content.startsWith(Config.prefix)) return
   Message.cmds(message)
@@ -42,6 +20,8 @@ bot.on('message', message => {
 bot.on('disconnected', () => {
   console.log('Bot Disconnected.')
 })
+
+bot.on('error', console.error)
 
 process.on('unhandledRejection', err => {
   if (err.toString().indexOf('Forbidden') > 0) return console.log('Bot cannot talk in this channel.')
